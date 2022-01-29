34c4d7e10c8ddf0c1918bcc564df4b33ad110235
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-01-28 18:32:59 -0800

    This is a proj temp.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+let answer = ''; 
+while (answer !== 'mellon') {
+  answer = prompt('Enter the passphrase to proceed...');
+}
+
+// for (let i = 0; i < 8; i++) {
+//     console.log(i);
+//   }
+
+
+for (let i = 1; i < 8; i++) {
+    console.log(i);
+}
+
+
+
+   if (a - b > 0) {
+    val = a - b;
+  } else {
+    val = b - a;
+  } 
+  
+
+
+   
